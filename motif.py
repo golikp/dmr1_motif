d8b945133c31e3f08818caeb802355bab3b2d31a
golikp <46810318+golikp@users.noreply.github.com>
2019-01-18 09:42:29 +0100

    Update motif.py

--- motif.py
+++ motif.py
@@ -12,7 +12,6 @@ print ("For motifs of ", motlen, " nt, in cer, par, mik, kud, bay, Cgla")
 for pos in range (len(Scer) - motlen + 1):
     motif = Scer[pos:pos + motlen]
     cons = Scer.count(motif) * Spar.count(motif) * Smik.count(motif) * Skud.count(motif) * Sbay.count(motif)
-#    if (cons > 0) & (Cgla.count(motif) == 0) & (Spar.count(motif) > Sbay.count(motif)) & (Scer.count(motif) > Sbay.count(motif)) :
-    if (cons > 0) & (Cgla.count(motif) == 0) & (Scer.count(motif) > Sbay.count(motif)) :
+    if (cons > 0) & (Cgla.count(motif) == 0) & (Scer.count(motif) > 1) :
         print ("Motif ",motif," at ", pos, Scer.count(motif), Spar.count(motif), Smik.count(motif), Skud.count(motif), Sbay.count(motif), Cgla.count(motif))
 
